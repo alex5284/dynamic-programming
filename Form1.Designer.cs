55801d160bee8f29d68059994944a64bbfbf8cfd
ark-l <ark-l@ukr.net>
2024-11-16 01:04:39 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnCreate2 = new System.Windows.Forms.Button();
+            this.tbm = new System.Windows.Forms.TextBox();
+            this.tbn = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.dgMatrix = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(746, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(559, 524);
+            this.listBox1.TabIndex = 15;
+            // 
+            // btnCreate2
+            // 
+            this.btnCreate2.Location = new System.Drawing.Point(295, 383);
+            this.btnCreate2.Name = "btnCreate2";
+            this.btnCreate2.Size = new System.Drawing.Size(114, 39);
+            this.btnCreate2.TabIndex = 14;
+            this.btnCreate2.Text = "Створити";
+            this.btnCreate2.UseVisualStyleBackColor = true;
+            this.btnCreate2.Click += new System.EventHandler(this.btnCreate2_Click_1);
+            // 
+            // tbm
+            // 
+            this.tbm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbm.Location = new System.Drawing.Point(207, 438);
+            this.tbm.Name = "tbm";
+            this.tbm.Size = new System.Drawing.Size(49, 24);
+            this.tbm.TabIndex = 12;
+            this.tbm.Text = "4";
+            // 
+            // tbn
+            // 
+            this.tbn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbn.Location = new System.Drawing.Point(207, 389);
+            this.tbn.Name = "tbn";
+            this.tbn.Size = new System.Drawing.Size(50, 24);
+            this.tbn.TabIndex = 11;
+            this.tbn.Text = "4";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(295, 438);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(114, 39);
+            this.btnCalc.TabIndex = 10;
+            this.btnCalc.Text = "Обрахувати";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click_1);
+            // 
+            // dgMatrix
+            // 
+            this.dgMatrix.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgMatrix.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMatrix.Location = new System.Drawing.Point(12, 21);
+            this.dgMatrix.Name = "dgMatrix";
+            this.dgMatrix.RowHeadersWidth = 51;
+            this.dgMatrix.RowTemplate.Height = 24;
+            this.dgMatrix.Size = new System.Drawing.Size(711, 319);
+            this.dgMatrix.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(41, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 18);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Кількість вкладень";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(24, 438);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Кількість підприємств";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1314, 571);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCreate2);
+            this.Controls.Add(this.tbm);
+            this.Controls.Add(this.tbn);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.dgMatrix);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCreate2;
+        private System.Windows.Forms.TextBox tbm;
+        private System.Windows.Forms.TextBox tbn;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.DataGridView dgMatrix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int n, m;
+        double[,] ReadMatrixFromGrid()
+        {
+            n = Convert.ToInt32(tbn.Text);
+            m = Convert.ToInt32(tbm.Text);
+            m++;
+            double[,] matrix = new double[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = Convert.ToDouble(dgMatrix.Rows[i].Cells[j].Value);
+                }
+            }
+            return matrix;
+        }
+
+        void Calc()
+        {
+            double[,] matrix = ReadMatrixFromGrid();
+            List<double> invest = new List<double>();
+            List<double> invest1 = new List<double>();
+            invest1.Add(0);
+            List<List<double>> P_n = new List<List<double>>();
+            int c = 1;
+            for (int i = 0; i < n; i++)
+            {
+                invest.Add(matrix[i, 0]);
+                invest1.Add(matrix[i, 0]);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                List<double> P_i = new List<double>();
+                if (c < m)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        P_i.Add(matrix[j, c]);
+                    }
+                    P_n.Add(P_i);
+                    c++;
+                }
+            }
+            List<Tuple<List<double>, List<double>>> tuples = new List<Tuple<List<double>, List<double>>>();
+            Tuple<List<double>, List<double>> t = new Tuple<List<double>, List<double>>(invest, P_n[0]);
+            tuples.Add(t);
+            c = 1;
+            int c2 = 0;
+            for (int k = 0; k < P_n.Count - 1; k++)
+            {
+
+                double[,] m = new double[invest.Count, invest.Count + 1];
+                for (int i = 0; i < invest.Count; i++)
+                {
+                    for (int j = 0; j < invest1.Count; j++)
+                    {
+                        if (j == 0) m[i, j] = tuples[k].Item2[i];
+                        else
+                        {
+                            double res = invest[i] - invest1[j];
+                            if (res < 0) break;
+                            else
+                            {
+                                if (res == 0) m[i, j] = P_n[c][i];
+                                else
+                                {
+                                    m[i, j] = P_n[c][j - 1] + tuples[k].Item2[c2];
+                                    c2--;
+                                }
+                            }
+                        }
+                    }
+                    c2 = i;
+                }
+                c++;
+
+                List<double> temp1 = new List<double>(), temp2 = new List<double>();
+                for (int i = 0; i < invest.Count; i++)
+                {
+                    List<double> temp = new List<double>();
+                    for (int j = 0; j < invest1.Count; j++)
+                    {
+                        temp.Add(m[i, j]);
+                    }
+                    temp1.Add(temp.Max());
+                    temp2.Add(invest1[temp.IndexOf(temp.Max())]);
+                }
+                t = new Tuple<List<double>, List<double>>(temp2, temp1);
+                tuples.Add(t);
+            }
+
+            double max_number = tuples[tuples.Count - 1].Item2.Max();
+            int index = tuples[tuples.Count - 1].Item2.IndexOf(max_number);
+            double max_index = tuples[tuples.Count - 1].Item1[index];
+            double ostatok = invest.Max() - max_index;//60
+            List<double> number = new List<double>();
+            List<double> ind = new List<double>();
+            ind.Add(max_index);
+            c = 0;
+            for (int i = tuples.Count - 2; i >= 0; i--)
+            {
+                int k = invest.IndexOf(ostatok);
+                double n = tuples[i].Item2.GetRange(0, k + 1).Max();
+                index = tuples[i].Item2.IndexOf(n);
+                double inde = tuples[i].Item1[index];
+                ostatok = ostatok - inde;
+                number.Add(n); ind.Add(inde);
+                if (ostatok == 0 && i != 0)
+                {
+                    c = i;
+                    do
+                    {
+                        ind.Add(0);
+                        c--;
+                    } while (c != 0);
+                    break;
+                }
+            }
+
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Максимальний приріст випуску продукції на " + tuples.Count + " підприємствах при розподілі");
+            listBox1.Items.Add("між ними " + invest.Max() + " тис.грн складає " + max_number + " тис.грн і буде отриманий якщо:");
+            listBox1.Items.Add("");
+            c = 1;
+            for (int i = ind.Count - 1; i >= 0; i--)
+            {
+                if (ind[i] == 0) listBox1.Items.Add(c + " підприємству коштів не виділяти");
+                else listBox1.Items.Add(c + " підприємству виділити " + ind[i]);
+                c++;
+            }
+        }
+
+        private void btnCalc_Click_1(object sender, EventArgs e)
+        {
+            Calc();
+        }
+
+        private void btnCreate2_Click_1(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(tbn.Text);
+            m = Convert.ToInt32(tbm.Text);
+            m++;
+            dgMatrix.Rows.Clear();
+            dgMatrix.Columns.Clear();
+            int i = 0;
+            for (i = 0; i < m; i++)
+            {
+                if (i == 0) dgMatrix.Columns.Add("X", "X");
+                else dgMatrix.Columns.Add("g" + i.ToString() + "(x)", "g" + i.ToString() + "(x)");
+            }
+
+            dgMatrix.Rows.Add(n);
+        }
+    }
+}
